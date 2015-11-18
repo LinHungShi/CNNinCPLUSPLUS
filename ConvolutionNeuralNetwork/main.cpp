51e2ebe3f02f75e751564d031ba8277fb8c31e31
LinHungShi <u9600114@gmail.com>
2015-11-18 22:58:49 +0800

    Initial Commit

---
+++ ConvolutionNeuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConvolutionNeuralNetwork
+//
+//  Created by Lin Hung-Shi on 11/18/15.
+//  Copyright © 2015 Lin Hung-Shi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
